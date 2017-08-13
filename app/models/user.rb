5baf6274cc724791fd78345bb4b250d6fc6a219d
Matthew <mcataloe@gmail.com>
2017-08-13 23:53:42 +0000

    Added form to concert page for logged in user

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,10 @@ class User < ApplicationRecord
   has_secure_password
   has_many :tickets, class_name: "Attendance", foreign_key: :attendee_id
   
-  validates :first_name, presence:true
-  validates :last_name, presence:true
-  validates :email, presence:true
-  validates :password, presence:true
+  validates :first_name, :last_name, :email, :password, presence:true
+
+  def full_name
+    "#{self.first_name} #{self.last_name}"
+  end
   
 end
