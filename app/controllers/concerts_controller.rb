7e306bc38c6bdb80b3b6003d07a0f42d35ebde39
Matthew <mcataloe@gmail.com>
2017-08-13 02:17:55 +0000

    Copied README from assignment to this file for referencing.  Set up views for index and show of concert along with controllers.

--- app/controllers/concerts_controller.rb
+++ app/controllers/concerts_controller.rb
@@ -1,5 +1,8 @@
 class ConcertsController < ApplicationController
+  before_action :set_concert, only: [:show, :edit, :update, :destroy]
+  
   def index
+    @concerts = Concert.all.order(:created_at)
   end
   
   def show
@@ -9,17 +12,32 @@ class ConcertsController < ApplicationController
   end
   
   def new
+    @concert = Concert.new
   end
   
   def create
+    # double check this line for the params.  Not sure if it's right or not
+    @concert = Concert.new(params[:concert]) 
   end
   
   def update
+    # double check this line as well :-S.
+    @concert.update(concert_params)
   end
   
   def destroy
+    @concert.destroy
+    
+    redirect_to concerts_path
   end
   
+  private
+  def set_concert
+    @concert ||= Concert.find(params[:id])
+  end
   
+  def concert_params
+    params.require(:concert).permit(:band, :venue, :date, :start_time)
+  end
   
 end
--- app/models/concert.rb
+++ app/models/concert.rb
@@ -1,4 +1,10 @@
 class Concert < ApplicationRecord
-  has_many :attendance_records, class_name: Attendance
+  has_many :attendance_records, class_name: "Attendance"
   has_many :attendees, through: :attendance_records
+  
+  validates :band, presence: true
+  validates :venue, presence: true
+  validates :date, presence: true
+  validates :start_time, presence: true
+  
 end
