c9eb5436b2a9a224561a3fe69c9ddaeab8d73aef
Matthew <mcataloe@gmail.com>
2017-08-14 13:30:01 +0000

    Completed initial skeleton of route for update on concert attendance

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
   has_many :tickets, class_name: "Attendance", foreign_key: :attendee_id
   
   validates :first_name, :last_name, :email, :password, presence:true
+  validates :email, uniqueness: true
 
   def full_name
     "#{self.first_name} #{self.last_name}"
